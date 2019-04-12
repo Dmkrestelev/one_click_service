93eb61aca392dfcaa084552d5d644cee78d2b12b
Дмитрий <>
2019-04-12 22:14:41 +0400

    naaa

--- app/api/api.py
+++ app/api/api.py
@@ -1,17 +1,20 @@
 import json
 from flask import request
 from app.api import bp
+from app.api.helper import crossdomain
 from app.models import City, Company, ServicesType, Request
 from flask import jsonify
 
 
 @bp.route('/city/', methods=['GET'])
+@crossdomain(origin='*')
 def city_all():
     cities = City.all()
     return json.dumps({'city': City.serialize_list(cities)}), 200, {'ContentType': 'application/json'}
 
 
 @bp.route('/company/', methods=['POST'])
+@crossdomain(origin='*')
 def company_create():
     if not request.json or not 'name' in request.json:
         return json.dumps({'error': 'incorrect_params'}), 400, {'ContentType': 'application/json'}
@@ -22,18 +25,21 @@ def company_create():
 
 
 @bp.route('/company/', methods=['GET'])
+@crossdomain(origin='*')
 def company_all():
     company = Company.all()
     return json.dumps({'companies': Company.serialize_list(company)}), 200, {'ContentType': 'application/json'}
 
 
 @bp.route('/services/', methods=['GET'])
+@crossdomain(origin='*')
 def types_all():
     services = ServicesType.all()
     return json.dumps({'services': ServicesType.serialize_list(services)}), 200, {'ContentType': 'application/json'}
 
 
 @bp.route('/request/', methods=['POST'])
+@crossdomain(origin='*')
 def request_create():
     if not request.json or not 'description' in request.json or not 'service_id' in request.json:
         return json.dumps({'error': 'incorrect_params'}), 400, {'ContentType': 'application/json'}
@@ -45,6 +51,7 @@ def request_create():
 
 
 @bp.route('/request/<int:id>', methods=['PUT'])
+@crossdomain(origin='*')
 def request_update(id):
     if not id:
         return json.dumps({'error': 'incorrect_params'}), 400, {'ContentType': 'application/json'}
@@ -55,6 +62,7 @@ def request_update(id):
 
 
 @bp.route('/request/', methods=['GET'])
+@crossdomain(origin='*')
 def request_all():
     requests = Request.all()
     return json.dumps({'services': Request.serialize_list(requests)}), 200, {'ContentType': 'application/json'}
